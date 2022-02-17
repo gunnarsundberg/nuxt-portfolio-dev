dd9a9d85a515156c1f80e86aef9f464a1ae45fc1
Gunnar Sundberg <gunnarsundberg@pm.me>
2022-02-17 16:42:27 -0500

    add my tech stack

--- components/TechStack.vue
+++ components/TechStack.vue
@@ -1,20 +1,23 @@
 <template>
-  <div class="mt-16 md:mt-24">
+  <div class="mt-8 md:mt-16">
     <div class="flex justify-center items-center text-base font-semibold text-gray-600 dark:text-gray-300">
-      <h2 class="text-center">Tech stack I use </h2>
+      <h2 class="text-center">Favorite Tools</h2>
       <IconDoubleDown class="h-4 w-4"/>
     </div>
 
 
-    <div class="flex flex-wrap justify-center items-center text-4xl mt-5">
+    <div class="flex flex-wrap justify-center items-center text-4xl mt-5 dark:text-gray-300">
       <IconPython class="m-2 md:m-4"/>
-      <IconDjango class="m-2 md:m-4"/>
-      <IconAws class="m-2 md:m-4"/>
-      <IconHeroku class="m-2 md:m-4"/>
-      <IconJavascript class="m-2 md:m-4"/>
-      <IconTailwind class="m-2 md:m-4"/>
-      <IconVuejs class="m-2 md:m-4"/>
-      <IconNuxt class="m-2 md:m-4"/>
+      <IconSQL class="m-2 md:m-4"/>
+      <IconR class="m-2 md:m-4"/>
+      <IconKeras class="m-2 md:m-4"/>
+      <IconTensorflow class="m-2 md:m-4"/>
+      <IconJupyter class="m-2 md:m-4"/>
+      <IconPandas class="m-2 md:m-4"/>
+      <IconDocker class="m-2 md:m-4"/>
+      <IconTerminal class="m-2 md:m-4"/>
+      <IconMacOS class="m-2 md:m-4"/>
+      <IconGithub class="m-2 md:m-4"/>
     </div>
   </div>
 </template>
